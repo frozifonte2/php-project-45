1756cf64db219104bb4cd6d736879f21576c8fda
AlexDeveloper <ololoaleks@yandex.ru>
2023-12-24 10:52:01 +0300

    Step 3

---
+++ src/Cli.php
@@ -0,0 +1,11 @@
+<?php
+namespace Php\Project\Cli;
+use function cli\line;
+use function cli\prompt;
+
+function getUserName()
+{
+	line('Welcome to the Brain Games!');
+	$name = prompt('May I have you name?');
+	line('Hello, %s!', $name);
+}
